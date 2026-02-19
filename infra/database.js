a8d43c9172f46d0d9a9ecd83bcacbdf0551b239d
Rodrigo Rosa <rodrigo@wonder.com.br>
2026-02-18 22:23:43 -0300

    Adiciona configuração de ssl para tentar usar supabase em prod

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false }
+        : false,
   });
 
   try {
